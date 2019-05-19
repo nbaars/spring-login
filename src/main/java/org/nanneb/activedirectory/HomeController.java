2a723282bbe0001bf5601d4eadc1dc8ff25054d7
Nanne Baars <nanneb@gmail.com>
2019-05-19 13:44:40 +0200

    Initial setup

---
+++ src/main/java/org/nanneb/activedirectory/HomeController.java
@@ -0,0 +1,14 @@
+package org.nanneb.activedirectory;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        return "index";
+    }
+}
